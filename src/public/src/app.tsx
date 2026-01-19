c6aa04fb7351bcfc8e18c3e90d4e6afa647df5be
karishma9142 <karishmacair47050@gmail.com>
2026-01-19 16:34:14 +0530

    ....

--- src/public/src/app.tsx
+++ src/public/src/app.tsx
@@ -5,9 +5,7 @@ export function App() {
 
   return (
     <>
-    <br />
       <Button  startIcon={<PlusIcon size="md"/>} varient="primary" size="md" text="Share Brain" onclick={()=>{}}></Button>
-      <br />
       <Button varient="secondary" size="md" text="Add Content" onclick={()=>{}}></Button>
     </>
   )
